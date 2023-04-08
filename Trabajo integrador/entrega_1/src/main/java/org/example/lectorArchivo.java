1458fc4f65a82ef3d3f49b51cb4308d640b235e3
frank <frankmolinaj9604@hotmail.com>
2023-04-08 18:30:50 -0300

    "asd"

--- Trabajo integrador/entrega_1/src/main/java/org/example/lectorArchivo.java
+++ Trabajo integrador/entrega_1/src/main/java/org/example/lectorArchivo.java
@@ -4,6 +4,8 @@ import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class lectorArchivo {
@@ -13,16 +15,18 @@ public class lectorArchivo {
     private List<resultadoType> resultado;
 
     public lectorArchivo(String rutaArchivo1, String rutaArchivo2) {
-
+        Path path = Paths.get("");
+        String directoryName = path.toAbsolutePath().toString()+"\\Trabajo integrador\\entrega_1\\";
+        System.out.println(directoryName);
         try {
 
-            pronostico = new CsvToBeanBuilder(new FileReader(rutaArchivo1))
+            pronostico = new CsvToBeanBuilder(new FileReader(directoryName+rutaArchivo1))
                     .withSkipLines(1)
                     .withSeparator(';')
                     .withType(pronosticoType.class)
                     .build()
                     .parse();
-            resultado = new CsvToBeanBuilder(new FileReader(rutaArchivo2))
+            resultado = new CsvToBeanBuilder(new FileReader(directoryName+rutaArchivo2))
                     .withSkipLines(1)
                     .withSeparator(';')
                     .withType(resultadoType.class)
